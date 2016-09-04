1e965984aaae6c5880600165086d4c6e4121f26e
brainstorm6334 <ttt287@nyu.edu>
2016-09-04 16:15:22 +0400

    Initial Commit

---
+++ CSO IDE/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSO IDE
+//
+//  Created by Theodore Tenev on 9/4/16.
+//  Copyright © 2016 Theodore Tenev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
